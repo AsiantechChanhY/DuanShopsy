81d796ed2da2c0aa99e0618c7023ca33ade66cd7
chanhy <tnycit1993@gmail.com>
2016-04-30 09:11:55 +0700

    add file

--- app/src/main/java/com/example/chanhy/shopsy/SplashPage.java
+++ app/src/main/java/com/example/chanhy/shopsy/SplashPage.java
@@ -12,7 +12,7 @@ public class SplashPage extends Activity {
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.favorite_popup);
+		setContentView(R.layout.recommended_tab);
 
 	}
 }
